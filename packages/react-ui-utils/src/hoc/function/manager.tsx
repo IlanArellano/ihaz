67370b606cd4699b738e05bd0fabc3386c283e40
Ilan Arellano <72232853+IlanArellano@users.noreply.github.com>
2024-11-22 12:32:40 -0600

    Update manager.tsx

--- packages/react-ui-utils/src/hoc/function/manager.tsx
+++ packages/react-ui-utils/src/hoc/function/manager.tsx
@@ -33,42 +33,45 @@ export function createFunctionalInstance<
     return getMap().get(key)! as IMethods[IKey];
   };
   return (props: P) => {
-    const set: FunctionalManagerMethods<Methods>["set"] = (key, value) => {
-      if (!value || !key) return;
-      if (!(value instanceof Function))
-        throw new Error(
-          `set prop method only allow Functions but it provides ${
-            Array.isArray(value) ? "array" : typeof value
-          } type`
-        );
-      const _map = getMap();
+    const set: FunctionalManagerMethods<Methods>["set"] = React.useCallback(
+      (key, value) => {
+        if (!value || !key) return;
+        if (!(value instanceof Function))
+          throw new Error(
+            `set prop method only allow Functions but it provides ${
+              Array.isArray(value) ? "array" : typeof value
+            } type`
+          );
+        const _map = getMap();
 
-      _map.set(key as any, value as any);
-      if (entries[key as keyof IMethods]) return;
-      Object.assign(entries as object, {
-        [key]: function () {
-          if (override && override[key as keyof typeof override])
-            return override[key as keyof typeof override].call(
-              null,
-              get,
-              isInstanceMounted,
-              ...[].slice.call(arguments)
-            );
-          if (!isInstanceMounted()) {
-            const displayName: string = (Comp as any)?.displayName;
-            throw new Error(
-              `Cannot Execute Method '${key as string}' ${
-                displayName
-                  ? `from uncontrolled component '${displayName}'`
-                  : ""
-              } because the Parent component doesn´t exists in React Tree`
-            );
-          }
-          const func = get(key as keyof IMethods);
-          return (func as Function).apply(null, arguments);
-        },
-      });
-    };
+        _map.set(key as any, value as any);
+        if (entries[key as keyof IMethods]) return;
+        Object.assign(entries as object, {
+          [key]: function () {
+            if (override && override[key as keyof typeof override])
+              return override[key as keyof typeof override].call(
+                null,
+                get,
+                isInstanceMounted,
+                ...[].slice.call(arguments)
+              );
+            if (!isInstanceMounted()) {
+              const displayName: string = (Comp as any)?.displayName;
+              throw new Error(
+                `Cannot Execute Method '${key as string}' ${
+                  displayName
+                    ? `from uncontrolled component '${displayName}'`
+                    : ""
+                } because the Parent component doesn´t exists in React Tree`
+              );
+            }
+            const func = get(key as keyof IMethods);
+            return (func as Function).apply(null, arguments);
+          },
+        });
+      },
+      []
+    );
 
     return (
       <>
